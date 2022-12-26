686fc6b3cee1ddba1d0a4e2d392cb929f9dd8820
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-12-26 18:10:45 -0500

    [feat] shapes

---
+++ src/lib/game/shapes.ts
@@ -0,0 +1,105 @@
+export type CellState = 0 | 1
+
+export type ShapeRotation = CellState[][]
+export type ActiveRotation = 0 | 1 | 2 | 3
+
+export type ShapeType = 'square' | 'line' | 'z' | 'triangle' | 'L'
+
+export interface Shape {
+	type: ShapeType
+	color: string
+	cells: ShapeRotation[]
+}
+
+export const shapes: Shape[] = [
+	{
+		type: 'triangle',
+		color: 'red',
+		cells: [
+			[
+				[0, 1, 0],
+				[1, 1, 1]
+			],
+			[
+				[1, 0],
+				[1, 1],
+				[1, 0]
+			],
+			[
+				[1, 1, 1],
+				[0, 1, 0]
+			],
+			[
+				[0, 1],
+				[1, 1],
+				[0, 1]
+			]
+		]
+	},
+	{
+		type: 'square',
+		color: 'green',
+		cells: [
+			[
+				[1, 1, 1],
+				[1, 1, 1],
+				[1, 1, 1]
+			]
+		]
+	},
+	{
+		type: 'line',
+		color: 'blue',
+		cells: [[[1, 1, 1, 1]], [[1], [1], [1], [1]]]
+	},
+	{
+		type: 'z',
+		color: 'yellow',
+		cells: [
+			[
+				[1, 1, 0],
+				[0, 1, 1]
+			],
+			[
+				[0, 1],
+				[1, 1],
+				[1, 0]
+			],
+			[
+				[0, 1, 1],
+				[1, 1, 0]
+			],
+			[
+				[1, 0],
+				[1, 1],
+				[0, 1]
+			]
+		]
+	},
+	{
+		type: 'L',
+		color: 'orange',
+		cells: [
+			[
+				[1, 0, 0],
+				[1, 1, 1]
+			],
+			[
+				[1, 1],
+				[1, 0],
+				[1, 0]
+			],
+			[
+				[1, 1, 1],
+				[0, 0, 1]
+			],
+			[
+				[0, 1],
+				[0, 1],
+				[1, 1]
+			]
+		]
+	}
+]
+
+export const randomShape = () => shapes[Math.floor(Math.random() * shapes.length)]

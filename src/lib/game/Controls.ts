663b1f5f14f4e18650302a6f53382809e7358ca1
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-12-27 00:12:39 -0500

    [wip] controls

---
+++ src/lib/game/Controls.ts
@@ -0,0 +1,57 @@
+import type { Game } from './Game'
+
+export class Controls {
+	active = false
+
+	constructor(public game: Game) {
+		if (typeof window === 'undefined') return
+
+		this.activate()
+	}
+
+	activate() {
+		if (this.active) return
+
+		window.addEventListener('keydown', this.keydown.bind(this))
+		this.active = true
+	}
+
+	moveBlockRight() {
+		this.game.grid.moveBlock('right')
+	}
+
+	moveBlockLeft() {
+		this.game.grid.moveBlock('left')
+	}
+
+	dropBlock() {
+		this.game.grid.dropBlock()
+	}
+
+	rotateBlock() {
+		this.game.grid.rotateBlock()
+	}
+
+	keydown(e: KeyboardEvent) {
+		if (this.game.gameOver) return
+
+		switch (e.key) {
+			case 'ArrowLeft':
+				this.moveBlockLeft()
+				break
+			case 'ArrowRight':
+				this.moveBlockRight()
+				break
+			case 'ArrowDown':
+				this.dropBlock()
+				break
+			case 'ArrowUp':
+				this.rotateBlock()
+				break
+		}
+	}
+
+	dispose() {
+		window?.removeEventListener('keydown', this.keydown)
+	}
+}
